2b9f9b70797fbef764e28d645e5696a6263e94ab
Ahmed <ahmedehab1677@gmail.com>
2026-06-15 01:39:53 +0300

    Update app.js to add form handling for product submission and redirect after processing. Remove unnecessary middleware and streamline request handling.

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ import express from "express";
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("This always runs!");
-  next();
-});
+app.use(express.urlencoded());
 app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware");
-  res.send(`<h1>The "Add Product" Page</h1>`);
+  res.send(`<form action="/product" method="POST">
+            <input type="text" name="title" />
+            <button type="submit">Submit</button>
+            </form>`);
+});
+app.use("/product", (req, res, next) => {
+  console.log(req.body.title);
+  res.redirect(`/`);
 });
 app.use("/", (req, res, next) => {
-  console.log("In another middleware");
   res.send(`<h1>Hello from Express</h1>`);
 });
 
