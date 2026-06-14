003746362f27b19be5c313ba9561cda741ff83d0
Ahmed <ahmedehab1677@gmail.com>
2026-06-15 01:33:15 +0300

    Refactor app.js to implement middleware for handling requests, adding routes for the "Add Product" page and a default response. Replace server creation with app.listen for Express integration.

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-import { createServer } from "http";
-import routes from "./routes.js";
 import express from "express";
 
 const app = express();
-const server = createServer(app);
 
-server.listen(3000);
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
+  next();
+});
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleware");
+  res.send(`<h1>The "Add Product" Page</h1>`);
+});
+app.use("/", (req, res, next) => {
+  console.log("In another middleware");
+  res.send(`<h1>Hello from Express</h1>`);
+});
+
+app.listen(3000);
