61fb06de991bcdca720d5f37d95742124e1baf16
Ahmed <ahmedehab1677@gmail.com>
2026-06-14 23:08:43 +0300

    Explore req and res methods, add a form to test routing

--- first-app.js
+++ first-app.js
@@ -1,8 +1,27 @@
-const http = require("http");
+import { createServer } from "http";
 
-const server = http.createServer((req, res) => {
-  console.log(req);
+const server = createServer((req, res) => {
+  console.log("req url =>", req.url);
+  console.log("req method =>", req.method);
+  console.log("req headers =>", req.heades);
+
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(`<body>
+                <form action="/message" method="POST">
+                  <input type="text" name="message" />
+                  <button type="submit">Submit</button>
+                </form>
+              </body>
+            `);
+    res.write("</html>");
+    return res.end();
+  }
+
   res.setHeader("Content-Type", "text/html");
+
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
   res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
