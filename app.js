ac0266152a24a1236a507697108d456237ea577e
Ahmed <ahmedehab1677@gmail.com>
2026-06-15 15:52:33 +0300

    Enhance app.js by adding a 404 error handling middleware and updating the admin routes to include a prefix. This improves user experience by providing a clear response for non-existent pages.

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ import shopRoutes from "./routes/shop.js";
 const app = express();
 
 app.use(express.urlencoded());
-app.use(adminRoutes);
+app.use("/admin", adminRoutes); // each route here is /admin/...
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found!</h1>");
+});
 
 app.listen(3000);
