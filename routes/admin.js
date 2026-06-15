090ca36d0740589e2d3e43434ec88ce19c101a60
Ahmed <ahmedehab1677@gmail.com>
2026-06-15 16:41:46 +0300

    Add HTML views for product management and shop display, updating routes to serve these files. The 'add-product' form is now served from a dedicated HTML file, and the shop page has been created to list products.

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,13 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const router = express.Router();
+const __filename = fileURLToPath(import.meta.url); // return the url of current file and convert it to normal path
+const __dirname = path.dirname(__filename); // extract the path of the folder
 
 router.use("/add-product", (req, res, next) => {
-  res.send(`<form action="/product" method="POST">
-                <input type="text" name="title" />
-                <button type="submit">Submit</button>
-            </form>`);
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,13 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const router = express.Router();
+const __filename = fileURLToPath(import.meta.url); // return the url of current file and convert it to normal path
+const __dirname = path.dirname(__filename); // extract the path of the folder
 
 router.get("/", (req, res, next) => {
-  res.send(`<h1>Hello from Express</h1>`);
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 export default router;
