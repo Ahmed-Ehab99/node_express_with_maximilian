b06579605d3d0d0410496871fe1c6a4c220404ef
Ahmed <ahmedehab1677@gmail.com>
2026-06-15 17:48:51 +0300

    Add CSS styles for main layout and product forms, enhancing the visual presentation of the application. Updated HTML views to include styles and improved structure for better user navigation.

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ import rootDir from "./utils/path.js";
 const app = express();
 
 app.use(express.urlencoded());
+app.use(express.static(path.join(rootDir, "public")));
+
 app.use("/admin", adminRoutes); // each route here is /admin/...
 app.use(shopRoutes);
-
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
