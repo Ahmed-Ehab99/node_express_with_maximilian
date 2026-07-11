f7ae3c5fd3e845df06c0dfcc3ce2fc68b556baa6
Ahmed <ahmedehab1677@gmail.com>
2026-07-11 16:14:36 +0300

    Refactor cart functionality to improve product retrieval and display. Update getCart method in User model to return product quantities correctly. Modify shop controller to streamline cart operations and enhance routing for cart access. Adjust cart view to reflect updated product data structure.

--- controllers/shop.js
+++ controllers/shop.js
@@ -37,20 +37,18 @@ exports.getIndex = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-// exports.getCart = (req, res, next) => {
-//   req.user
-//     .getCart()
-//     .then((cart) => {
-//       return cart.getProducts().then((products) => {
-//         res.render("shop/cart", {
-//           path: "/cart",
-//           pageTitle: "Your Cart",
-//           products,
-//         });
-//       });
-//     })
-//     .catch((err) => console.log(err));
-// };
+exports.getCart = (req, res, next) => {
+  req.user
+    .getCart()
+    .then((products) => {
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products,
+      });
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
@@ -58,38 +56,10 @@ exports.postCart = (req, res, next) => {
     .then((product) => {
       return req.user.addToCart(product);
     })
-    .then((result) => console.log(result));
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user
-  //   .getCart()
-  //   .then((cart) => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: prodId } });
-  //   })
-  //   .then((products) => {
-  //     let product;
-  //     if (products.length > 0) {
-  //       product = products[0];
-  //     }
-  //     if (product) {
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return product;
-  //     }
-  //     return Product.findByPk(prodId);
-  //   })
-  //   .then((product) => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: { quantity: newQuantity },
-  //     });
-  //   })
-  //   .then(() => {
-  //     res.redirect("/cart");
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
+    .then((result) => {
+      console.log(result);
+      res.redirect("/cart");
+    });
 };
 
 // exports.postCartDeleteProduct = (req, res, next) => {
